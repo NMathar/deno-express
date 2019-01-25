86156cf85812e0dd691376bfff7d5edcab72fd96
NMathar <8554558+NMathar@users.noreply.github.com>
2019-01-25 07:53:32 +0100

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,4 @@
+import * as path from "https://deno.land/x/fs/path.ts";
+import * as http from "https://deno.land/x/http/http.ts";
+import { red, yellow, cyan, green } from "https://deno.land/x/colors/mod.ts";
+export { path, http, red, yellow, cyan, green };
