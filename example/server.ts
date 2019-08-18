5e14cf9a13f03851bc7bda288ea06eca12d5fc9f
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-08-19 00:51:44 +0530

    Example added

---
+++ example/server.ts
@@ -0,0 +1,17 @@
+import * as expressive from "../mod.ts";
+
+(async () => {
+  const port = 3000;
+  const app = new expressive.App();
+  app.use(expressive.simpleLog());
+  app.use(expressive.static_("./public"));
+  app.use(expressive.bodyParser.json());
+  app.get("/api/todos", async (req, res) => {
+    await res.json([{ name: "Buy some milk" }]);
+  });
+  app.get("/api/user/{user_id}", async (req, res) => {
+    await res.json([{ id: req.params.user_id, name: "Jim Doe", phone: "12425323" }]);
+  });
+  const server = await app.listen(port);
+  console.log("app listening on port: " + server.port);
+})();
