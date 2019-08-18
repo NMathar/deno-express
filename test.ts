fdde67932cd87fa505e3592cb33aa13c1501ebf0
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-08-18 15:10:53 +0530

    updates with new standard library

--- test.ts
+++ test.ts
@@ -1,28 +1,39 @@
-import { test, assert, assertEqual } from "https://deno.land/x/testing/mod.ts";
+import { test, runTests } from "https://deno.land/std/testing/mod.ts";
+import { assert, assertEquals, assertThrows} from "https://deno.land/std/testing/asserts.ts"; 
 import { Request, simplePathMatcher } from "./mod.ts";
 
-test(function parse_url() {
-  const req = new Request({
-    url: "/files-tmb/1234/abc.png?key=val"
-  });
-  assertEqual(req.path, "/files-tmb/1234/abc.png");
-  assertEqual(req.query.key, "val");
-});
-
-test(function pathMatcher() {
-  assert(!!simplePathMatcher("/")("/"));
-  assert(!!simplePathMatcher("/foo")("/foo"));
-  assert(!!simplePathMatcher("/foo/")("/foo/"));
-  assert(!!simplePathMatcher("/foo/1")("/foo/1"));
-  assertEqual(simplePathMatcher("/foo")("/"), null);
-  assertEqual(simplePathMatcher("/foo")("/fooo"), null);
-  assertEqual(simplePathMatcher("/foo")("/foo/"), null);
-  assertEqual(simplePathMatcher("/{a}")("/foo").a, "foo");
-  assertEqual(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").a, "34");
-  assertEqual(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").xxx, "1");
-  for (let v of ["//", "/{}", "/{x}/{x}"]) {
-    assert.throws(() => {
-      simplePathMatcher(v);
-    });
+test({
+  name: "Testing function parse_url",
+  fn(): void {
+    {
+      const req = new Request({
+        url: "/files-tmb/1234/abc.png?key=val"
+      });
+      assertEquals(req.path, "/files-tmb/1234/abc.png");
+      assertEquals(req.query.key, "val");
+    }
   }
 });
+
+test({
+  name: "Testing function simplePathMatcher",
+  fn(): void {
+    assert(!!simplePathMatcher("/")("/"));
+    assert(!!simplePathMatcher("/foo")("/foo"));
+    assert(!!simplePathMatcher("/foo/")("/foo/"));
+    assert(!!simplePathMatcher("/foo/1")("/foo/1"));
+    assertEquals(simplePathMatcher("/foo")("/"), null);
+    assertEquals(simplePathMatcher("/foo")("/fooo"), null);
+    assertEquals(simplePathMatcher("/foo")("/foo/"), null);
+    assertEquals(simplePathMatcher("/{a}")("/foo").a, "foo");
+    assertEquals(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").a, "34");
+    assertEquals(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").xxx, "1");
+    for (let v of ["//", "/{}", "/{x}/{x}"]) {
+      assertThrows(() => {
+        simplePathMatcher(v);
+      });
+    }
+  }
+});
+
+runTests();
