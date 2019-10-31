7d8a95973e4ec89d4ebb11174eedb82358e18841
NMathar <nicolai.mathar@ima.rwth-aachen.de>
2019-10-31 07:40:54 +0100

    change deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import * as path from "https://deno.land/std/fs/path.ts";
+import * as path from "https://deno.land/std/path/mod.ts";
 import * as http from "https://deno.land/std/http/server.ts";
 import { red, yellow, cyan, green } from "https://deno.land/std/fmt/colors.ts";
 export { path, http, red, yellow, cyan, green };
